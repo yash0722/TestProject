4f38975cbe7014aabf23c930bb4383125cf16e84
paulomi2022 <react1.jstechno@gmail.com>
2024-05-13 11:05:28 +0530

    InLogin Button Add

--- src/component/screen/Login.tsx
+++ src/component/screen/Login.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 const Login = ()=>{
     return(
         <View>
             <Text>This is Login page</Text>
+            <Button title="Submit"/>
         </View>
     );
 };
